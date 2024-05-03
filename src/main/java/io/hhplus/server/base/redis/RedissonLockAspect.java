80e44931560c1a436e46a23e2b2230f7e1a15cb1
cseungm <120002312+fittcha@users.noreply.github.com>
2024-05-03 14:11:34 +0900

    feat: 유저 포인트 충전 및 사용 - Redisson 적용

--- src/main/java/io/hhplus/server/base/redis/RedissonLockAspect.java
+++ src/main/java/io/hhplus/server/base/redis/RedissonLockAspect.java
@@ -1,31 +1,37 @@
 package io.hhplus.server.base.redis;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Component;
 
+import java.lang.reflect.Method;
 import java.util.concurrent.TimeUnit;
 
 @Aspect
 @Component
+@RequiredArgsConstructor
+@Slf4j
 public class RedissonLockAspect {
 
     private final RedissonClient redissonClient;
 
-    public RedissonLockAspect(RedissonClient redissonClient) {
-        this.redissonClient = redissonClient;
-    }
+    @Around("@annotation(io.hhplus.server.base.redis.RedissonLock)")
+    public Object lock(ProceedingJoinPoint joinPoint) throws Throwable {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Method method = signature.getMethod();
+        RedissonLock redissonLock = method.getAnnotation(RedissonLock.class);
 
-    @Around("@annotation(redissonLock)")
-    public Object around(ProceedingJoinPoint joinPoint, RedissonLock redissonLock) throws Throwable {
-        RLock lock = redissonClient.getLock(redissonLock.value());
+        RLock lock = redissonClient.getLock(redissonLock.key());
         try {
             // 락 취득 시도
-            boolean isLocked = lock.tryLock(10, 60, TimeUnit.SECONDS);
-            if (!isLocked) {
+            boolean available  = lock.tryLock(redissonLock.waitTime(), redissonLock.leaseTime(), redissonLock.timeUnit());
+            if (!available) {
                 throw new IllegalStateException("Unable to acquire lock");
             }
             return joinPoint.proceed();
